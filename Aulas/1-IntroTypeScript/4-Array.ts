b546f7fed9cdb74c1990e3a7d32804c60816c213
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2023-05-02 18:10:48 -0300

    Aula Array

---
+++ Aulas/1-IntroTypeScript/4-Array.ts
@@ -0,0 +1,15 @@
+const array: Array<number> = [1,2,3,4]
+
+const stringArray: string[] = ['a', 'b', 'c']
+
+//operador diamante <>
+
+console.log('array original', array)
+
+array.push(5)
+
+console.log('array com novo elemento', array)
+
+array.pop()
+
+console.log('array retirando último elemento', array)
